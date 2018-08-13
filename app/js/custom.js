fd48b5da241217e06043ed56554449211730b6df
Mike K <mikhail.karkachov@gmail.com>
2018-08-13 20:40:45 +0300

    added smooth scroll

--- app/js/custom.js
+++ app/js/custom.js
@@ -65,4 +65,7 @@ document.addEventListener("DOMContentLoaded", function () {
       };
     });
   }
+
+  // Smooth scroll
+  new SmoothScroll('.nav a');
 });
--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
 @import '../../node_modules/headroom.js/dist/headroom.min.js';
 @import '../../node_modules/tiny-slider/dist/min/tiny-slider.js';
+@import '../../node_modules/smooth-scroll/dist/smooth-scroll.min.js';
 @import 'custom.js';
